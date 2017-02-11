281c4d2487e5af193b3fc235c64d16d191dbca88
Kodyfwee <kodyfint@buffalo.edu>
2017-02-11 15:25:21 -0500

    Added 2 classes and started figuring out structure

---
+++ Race.java
@@ -0,0 +1,18 @@
+#import User;
+
+public class Race{
+
+    public User user1;
+    public User user2;
+
+    public Race(User u1,User u2){
+        user1 = u1;
+        user2 = u2;
+    }
+
+    public void startRace(){
+        
+    }  
+
+
+}
---
+++ User.java
@@ -0,0 +1,20 @@
+
+
+public class User{
+
+    String userName;
+    
+    public User(String name){
+        userName = name;
+    }
+
+    public String getUserName(){
+
+    }
+
+    public void setUserName(String name){
+        // Change on database the username first
+        
+        userName = name;
+    }
+}
