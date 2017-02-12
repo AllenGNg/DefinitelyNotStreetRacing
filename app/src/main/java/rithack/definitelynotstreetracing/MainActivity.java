12de9f6b33052d59a0bc3e4b241cfbcfa94c0ee2
Matthew Luck <mjluck@buffalo.edu>
2017-02-11 23:51:21 -0500

    Added Button to MainActivity, linked it to SignupActivity.

--- app/src/main/java/rithack/definitelynotstreetracing/MainActivity.java
+++ app/src/main/java/rithack/definitelynotstreetracing/MainActivity.java
@@ -1,15 +1,30 @@
 package rithack.definitelynotstreetracing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        startApp = (Button) findViewById(R.id.createAccount);
+
+        startApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(MainActivity.this, SignupActivity.class));
+            }
+        });
+
     }
 }
