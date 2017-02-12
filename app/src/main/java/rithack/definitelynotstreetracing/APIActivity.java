66552fd3ef9f038e8acea40c50e3701a16ed8989
Kodyfwee <kodyfint@buffalo.edu>
2017-02-12 01:00:43 -0500

    Almost fixed it. Still not instantitating correctly, but will figure it out

--- app/src/main/java/rithack/definitelynotstreetracing/APIActivity.java
+++ app/src/main/java/rithack/definitelynotstreetracing/APIActivity.java
@@ -1,8 +1,5 @@
 package rithack.definitelynotstreetracing;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -18,6 +15,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import java.util.Date;
 
@@ -31,46 +29,39 @@ public class APIActivity extends FragmentActivity
     public boolean mRequestingLocationUpdates = true;
     public String lastUpdateTime;
 
-    public APIActivity(){
+    public APIActivity() {
         Bundle temp = new Bundle();
         temp.putString("message", "this is a test");
         onCreate(temp);
-        onStart();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Create a GoogleApiClient instance
-
-        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information..
+        Log.d("tacos","i hate myself");
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
                 .build();
 
         // Now make a location request and set it up to handle high accuracy pinging
-
+        Log.d("fun","i hate myself");
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(1600);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        onStart();
     }
 
     protected void onStart() {
-        mGoogleApiClient.connect();
         super.onStart();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.start(mGoogleApiClient, getIndexApiAction());
+        mGoogleApiClient.connect();
     }
 
     protected void onStop() {
         mGoogleApiClient.disconnect();
-        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
-// See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(mGoogleApiClient, getIndexApiAction());
+        super.onStop();
     }
 
     @Override
@@ -104,7 +95,7 @@ public class APIActivity extends FragmentActivity
     public void onLocationChanged(Location location) {
         mCurrentLocation = location;
         //lastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        System.out.println(mCurrentLocation.toString());
+        Log.d("STATE",mCurrentLocation.toString());
     }
 
     public Location getLocation(){
@@ -119,19 +110,4 @@ public class APIActivity extends FragmentActivity
         System.exit(-1);
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("API Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
 }
--- app/src/main/java/rithack/definitelynotstreetracing/MapsActivity.java
+++ app/src/main/java/rithack/definitelynotstreetracing/MapsActivity.java
@@ -14,11 +14,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private APIActivity kody;
+    private GPSActivity kody;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        kody = new APIActivity();
+        kody = new GPSActivity();
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
