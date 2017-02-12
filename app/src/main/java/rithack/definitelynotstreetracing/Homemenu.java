10dfd34b17421518535d3515c4dd43af83c55009
Matthew Luck <mjluck@buffalo.edu>
2017-02-12 00:38:56 -0500

    Added pics and json to repository.

---
+++ app/src/main/java/rithack/definitelynotstreetracing/Homemenu.java
@@ -0,0 +1,13 @@
+package rithack.definitelynotstreetracing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Homemenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homemenu);
+    }
+}
